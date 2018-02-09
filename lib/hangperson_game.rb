039ed631f5ecf34fae32426b73f13eac9617b55e
Andrew Schlesinger <aschles4@uncc.edu>
2018-02-09 18:35:30 +0000

    Added some basic functionality to letter checking

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -2,14 +2,18 @@ class HangpersonGame
 
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
-
+  attr_accessor :word
+  attr_accessor :guesses 
+  attr_accessor :wrong_guesses
   # Get a word from remote "random word" service
-
   # def initialize()
   # end
   
+ 
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
@@ -24,4 +28,20 @@ class HangpersonGame
     }
   end
 
+   def guess(letter) 
+    if (letter.nil? || letter.empty? || letter =~ /[^a-zA-Z]+/)
+      raise (ArgumentError)
+    end
+    letter.downcase!
+    if(!guesses.include? letter) && (!wrong_guesses.include? letter)
+     if (word.include? letter) 
+       @guesses += letter
+     else
+       @wrong_guesses += letter
+     end
+       
+    else
+      return false
+    end
+  end
 end
